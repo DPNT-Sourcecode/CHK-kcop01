49c7037c4cc6554bbaf38b5720566bd8ae46dd4f
sbx_user1051 <sbx_user1051@169.254.16.61>
2025-08-14 10:46:58 +0000

    Thu Aug 14 10:46:58 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,7 +1,5 @@
 package io.accelerate.solutions.CHK;
 
-import io.accelerate.runner.SolutionNotImplementedException;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,8 +8,7 @@ public class CheckoutSolution {
     public static final int UNIT_PRICE_C = 20;
     public static final int UNIT_PRICE_D = 15;
     public static final int UNIT_PRICE_E = 40;
-    public static final int OFFER_PRICE_A = 130;
-    public static final int OFFER_PRICE_B = 45;
+    public static final int BUNDLE_PRICE_2B = 45;
     public static final int UNIT_PRICE_A = 50;
     public static final int UNIT_PRICE_B = 30;
     public static final int BUNDLE_PRICE_5A = 200;
@@ -45,20 +42,14 @@ public class CheckoutSolution {
         int freeB = countE / 2;
         countB = Math.max(0, countB - freeB);
 
-        totalPrice += getBundleOffer(countA,3, OFFER_PRICE_A, UNIT_PRICE_A);
-        totalPrice += getBundleOffer(countB,2, OFFER_PRICE_B, UNIT_PRICE_B);
+        totalPrice += priceForA(countA);
+        totalPrice += priceForB(countB);
         totalPrice += UNIT_PRICE_C * countC;
         totalPrice += UNIT_PRICE_D * countD;
         totalPrice += UNIT_PRICE_E * countE;
 
         return totalPrice;
 
-    }
-
-    private int getBundleOffer(int itemsBuying ,int offerQuantity, int offerPrice, int unitPrice) {
-        int offer = itemsBuying / offerQuantity;
-        int remaining = itemsBuying % offerQuantity;
-        return (offer * offerPrice) + (remaining * unitPrice);
     }
 
     private int priceForA(int countA) {
@@ -73,5 +64,16 @@ public class CheckoutSolution {
         return total;
     }
 
+    private int priceForB(int countB) {
+        int total = 0;
+        total += (countB / 2) * BUNDLE_PRICE_2B;
+        countB = countB % 2;
+
+        total += countB * UNIT_PRICE_B;
+        return total;
+
+    }
+
 }
+
 
